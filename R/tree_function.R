5d8ca4356daa353d61c8e199751e364fcc02fb08
kalebabram <abram.kaleb@gmail.com>
2024-09-03 14:42:23 -0400

    Updated tree_function.R to output cutoff in fn

--- R/tree_function.R
+++ R/tree_function.R
@@ -12,13 +12,7 @@
 #' treeFunc(grumpsObj)
 treeFunc <- function(grumpsObj){
   tr <- as.phylo(grumpsObj$hClust)
-  if (grumpsObj$mode == 'general'){
-    out_prefix <- paste('_',grumpsObj$cutoff,'_',grumpsObj$clusteringmethod,'_', sep = '')
-    outfile <- paste(strsplit(grumpsObj$file, '_distmat.csv')[[1]][1],'_r',out_prefix,'_newick.nwk', sep = '')
-  }
-  else{
-    out_prefix <- paste('_',grumpsObj$clusteringmethod, sep = '')
-    outfile <- paste(strsplit(grumpsObj$file, '_distmat.csv')[[1]][1],'_r',out_prefix,'_newick.nwk', sep = '')
-  }
+  out_prefix <- paste('_',grumpsObj$cutoff,'_',grumpsObj$clusteringmethod,'_', sep = '')
+  outfile <- paste(strsplit(grumpsObj$file, '_distmat.csv')[[1]][1],'_r',out_prefix,'_newick.nwk', sep = '')
   write.tree(tr, file = outfile)
 }
